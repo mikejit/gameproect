98fac15e4bb608a76d520560ef36321bcc632175
Francisco <franxa@gomes.pt>
2026-02-12 09:33:40 +0000

    Create main menu with interactive buttons
    Implement main menu with buttons for Play, Options, and Settings.

---
+++ JEUX/main.menu.py
@@ -0,0 +1,24 @@
+def main_menu():
+  pygame.display.set_caption:("Menu")
+
+  while True:
+    SCREEN.blit(BG,(0,0))
+
+    MENU_MOUSE_POS = pygame.mouse.get_pos()
+
+    MENU_TEXT = get_font(100).render("MAIN MENU", True, #FFFFFF)
+    MENU_RECT = MENU_TEXT.get_rect(center= (640, 100))
+
+    PLAY_BUTTON = Button(image=pygame.image.load(assests/Play Rect.png), pos=(640, 250),
+                        text_input="PLAY", font=get_font(75), base_color = #d7fcd4, hovering_color="White")
+    OPTION_BUTTON = Button(image=pygame.image.load(assests/Options Rect.png), pos=(640, 400),
+                        text_input="OPTIONS", font = get_font(75), base_color = #d7fcd4, hovering_color="White")
+    SETTINGS_BUTTON = Button(image=pygame.image.load(assests/Play Rect.png), pos(640, 550),
+                            text_input="SETTINGS", font = get_font(75), base_color = #d7fcd4, hovering_color="White")
+
+    SCREEN.blit(MENU_TEXT, MENU_RECT)
+                            
+    for button in [PLAY_BUTTON, OPTIONS_BUTTON, SEETINGS_BUTTON]:
+        button.changeColor(MENU_MOUSE_POS)
+        button.update(SCREEN)
+      
