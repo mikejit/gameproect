f01781d4eba547bf0906758462682e8eafc3bcaa
Francisco <franxa@gomes.pt>
2026-02-12 10:18:33 +0000

    Fix syntax and logic errors in menu functions

--- JEUX/main.menu.py
+++ JEUX/main.menu.py
@@ -39,7 +39,21 @@ def options():
         OPTIONS_RECT = OPTIONS_TEXT.get_rect(center=(640,260))
         SCREEN.blit(OPTIONS_TEXT, OPTIONS_RECT)
 
-        OPTIONS_BACK= Button(image=None, pos(640,460), 
+        OPTIONS_BACK= Button(image=None, pos(640,460),
+                            text_input="BACK", font =get_font(75), base_color="Black, hovering_color="Green")
+
+        OPTIONS_BACK.changeColor(OPTIONS_MOUSE_POS)
+        OPTIONS_BACK.update(SCREEN)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.qui()
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if OPTIONS_BACK.checkForInput(OPTIONS_MOUE_POS):
+                    main_menu()
+
+        pygame.display.update()
                              
         
 
@@ -88,6 +102,7 @@ def main_menu():
     pygame.display.update()
 
 main_menu()
+
 
 
 
