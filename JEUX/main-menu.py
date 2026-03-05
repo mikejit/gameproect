019925bfc8ac7f6e93576bb4bf444f470efca0eb
Francisco <franxa@gomes.pt>
2026-03-05 10:47:01 +0000

    Refactor main menu and related functions

--- JEUX/main-menu.py
+++ JEUX/main-menu.py
@@ -113,14 +113,14 @@ def main_menu():
         MENU_TEXT = get_font(100).render("MAIN MENU", True, "#FFFFFF")
         MENU_RECT = MENU_TEXT.get_rect(center= (640, 100))
 
-        PLAY_BUTTON = Button(image = pygame.image.load("assets/Play Rect.png"), pos=(640,250),
+        PLAY_BUTTON = Button(image = pygame.image.load("assets/Play 7eb10a21f253da18c8a7760e275590ad.jpg"), pos=(640,250),
                              text_input="PLAY", font= get_font(75), base_color= "##d7fcd4", hovering_color="White" )
         
-        OPTION_BUTTON = Button(image=pygame.image.load("assets/Options Rect.png"), pos=(640,400),
+        OPTION_BUTTON = Button(image=pygame.image.load("assets/Options 7eb10a21f253da18c8a7760e275590ad.jpg"), pos=(640,400),
                                text_input="OPTIONS", font=get_font(75), base_color="#d7fcd4", hoverinf_color="White")
-        SETTINGS_BUTTON = Button(image=pygame.image.load("assets/Settings Rect.png"), pos=(640,400),
+        SETTINGS_BUTTON = Button(image=pygame.image.load("assets/Settings 7eb10a21f253da18c8a7760e275590ad.jpg"), pos=(640,400),
                                text_input="SETTINGS", font=get_font(75), base_color="#d7fcd4", hoverinf_color="White")
-        QUIT_BUTTON = Button(image=pygame.image.load("assets/Quit Rect.png"), pos=(640,400),
+        QUIT_BUTTON = Button(image=pygame.image.load("assets/Quit 7eb10a21f253da18c8a7760e275590ad.jpg"), pos=(640,400),
                                text_input="QUIT", font=get_font(75), base_color="#d7fcd4", hoverinf_color="White")
         screen.blit(MENU_TEXT, MENU_RECT)
                             
@@ -145,4 +145,5 @@ def main_menu():
         pygame.display.update()
 
 main_menu()
+
 
